28d988246b37ce61554ec5306d0bc7125f775210
Justin Johnson <94727825+JJHelloo@users.noreply.github.com>
2025-07-07 15:12:48 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// If your repo is called "orbit-gallery", for example:
-const repoName = 'orbit-gallery';
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/orbit-gallery/',
   plugins: [react()],
-});
-
+})
